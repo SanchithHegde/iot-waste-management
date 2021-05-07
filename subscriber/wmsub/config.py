4bf75aec01c134a0df45af1aced3e7557b9cb257
Sanchith Hegde <sanchith.hegde01@gmail.com>
2021-05-07 21:26:01 +0530

    config: Remove commented out classes based on TypedDict
    Signed-off-by: Sanchith Hegde <sanchith.hegde01@gmail.com>

--- subscriber/wmsub/config.py
+++ subscriber/wmsub/config.py
@@ -5,23 +5,6 @@ from typing import Optional, Union
 PathType = Union[str, pathlib.Path]
 
 
-# class Ssl(TypedDict):
-#     ca_certs: PathType
-#     client_certs: PathType
-#     client_keys: PathType
-
-
-# class Mqtt(TypedDict):
-#     broker_url: str
-#     topic: str
-#     qos: int
-#     ssl: Optional[Ssl]
-
-
-# class Config(TypedDict):
-#     mqtt: Mqtt
-
-
 @dataclass
 class Ssl:
     ca_certs: PathType
