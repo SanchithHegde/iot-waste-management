2de8b3df8ccd9b555e462050dd9b42598e535aad
Sanchith Hegde <sanchith.hegde01@gmail.com>
2021-05-24 22:45:59 +0530

    Add SettingsActivity
    Signed-off-by: Sanchith Hegde <sanchith.hegde01@gmail.com>

--- android/app/src/main/java/me/sanchithhegde/wastecollection/MainActivity.kt
+++ android/app/src/main/java/me/sanchithhegde/wastecollection/MainActivity.kt
@@ -1,15 +1,19 @@
 package me.sanchithhegde.wastecollection
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.onesignal.OneSignal
-
-const val ONESIGNAL_APP_ID = BuildConfig.ONESIGNAL_APP_ID
+import me.sanchithhegde.wastecollection.BuildConfig.ONESIGNAL_APP_ID
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
 
         // Logging set to help debug issues, remove before releasing your app.
         OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
@@ -18,4 +22,22 @@ class MainActivity : AppCompatActivity() {
         OneSignal.initWithContext(this)
         OneSignal.setAppId(ONESIGNAL_APP_ID)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_settings -> {
+            startActivity(Intent(this, SettingsActivity::class.java))
+            true
+        }
+
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
 }
---
+++ android/app/src/main/java/me/sanchithhegde/wastecollection/SettingsActivity.kt
@@ -0,0 +1,34 @@
+package me.sanchithhegde.wastecollection
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+    }
+}
