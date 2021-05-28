cd21b6a6128383572050004285372861a5d07d95
Sanchith Hegde <sanchith.hegde01@gmail.com>
2021-05-28 12:24:32 +0530

    app: Convert Flow to LiveData in ViewModel
    This ensures that the UI is updated whenever the database is updated
    
    Signed-off-by: Sanchith Hegde <sanchith.hegde01@gmail.com>

--- android/app/src/main/java/me/sanchithhegde/wastecollection/MessageViewModel.kt
+++ android/app/src/main/java/me/sanchithhegde/wastecollection/MessageViewModel.kt
@@ -1,7 +1,9 @@
 package me.sanchithhegde.wastecollection
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import me.sanchithhegde.wastecollection.data.Message
 import me.sanchithhegde.wastecollection.data.MessageRepository
@@ -10,5 +12,11 @@ import javax.inject.Inject
 @HiltViewModel
 class MessageViewModel @Inject internal constructor(messageRepository: MessageRepository) :
     ViewModel() {
-    val messages: LiveData<List<Message>> = messageRepository.getAllMessages()
+    val messages: LiveData<List<Message>> = Transformations.map(
+        messageRepository.getAllMessages().asLiveData()
+    ) { messageList ->
+        messageList.map { messageEntity ->
+            messageEntity.toMessage(WasteCollectionApplication.instance.applicationContext)
+        }
+    }
 }
--- android/app/src/main/java/me/sanchithhegde/wastecollection/data/MessageRepository.kt
+++ android/app/src/main/java/me/sanchithhegde/wastecollection/data/MessageRepository.kt
@@ -1,22 +1,12 @@
 package me.sanchithhegde.wastecollection.data
 
-import androidx.lifecycle.Transformations
-import androidx.lifecycle.asLiveData
-import me.sanchithhegde.wastecollection.WasteCollectionApplication
 import java.time.Instant
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class MessageRepository @Inject constructor(private val messageDao: MessageDao) {
-    fun getAllMessages() =
-        Transformations.map(
-            messageDao.getAllMessagesDistinctUntilChanged().asLiveData()
-        ) { messageList ->
-            messageList.map { messageEntity ->
-                messageEntity.toMessage(WasteCollectionApplication.instance.applicationContext)
-            }
-        }
+    fun getAllMessages() = messageDao.getAllMessagesDistinctUntilChanged()
 
     fun insertMessage(title: String, body: String) {
         messageDao.insertMessage(MessageEntity(Instant.now().toEpochMilli(), title, body))
