908be29ef9f9e3bc4b50021c0fd84b3ad7515b9c
Sanchith Hegde <sanchith.hegde01@gmail.com>
2021-07-05 16:09:58 +0530

    build(deps): Update plugins
    - Android Gradle plugin to 4.2.2
    - Kotlin Gradle plugin to 1.5.20
    - OneSignal Gradle plugin to 0.13.4
    
    Signed-off-by: Sanchith Hegde <sanchith.hegde01@gmail.com>

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,10 +6,10 @@ buildscript {
   }
 
   dependencies {
-    classpath("com.android.tools.build:gradle:4.2.1")
-    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+    classpath("com.android.tools.build:gradle:4.2.2")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
     classpath("com.google.dagger:hilt-android-gradle-plugin:2.36")
-    classpath("gradle.plugin.com.onesignal:onesignal-gradle-plugin:0.12.10")
+    classpath("gradle.plugin.com.onesignal:onesignal-gradle-plugin:0.13.4")
   }
 }
 
