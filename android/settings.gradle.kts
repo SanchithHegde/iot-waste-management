23ff4612ea05472c88ce00b79fc7f82baa5b48a5
Sanchith Hegde <sanchith.hegde01@gmail.com>
2021-07-05 15:14:48 +0530

    gradle: Add extra artifact version key rules for refreshVersions
    Signed-off-by: Sanchith Hegde <sanchith.hegde01@gmail.com>

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -4,12 +4,16 @@ pluginManagement {
   }
 
   plugins {
-    id("de.fayard.refreshVersions") version "0.10.1"
+    id("de.fayard.refreshVersions").version("0.10.1")
   }
 }
 
 plugins {
   id("de.fayard.refreshVersions")
+}
+
+refreshVersions {
+  extraArtifactVersionKeyRules(file("refreshVersions-extra-rules.txt"))
 }
 
 rootProject.name = "Waste Collection"
