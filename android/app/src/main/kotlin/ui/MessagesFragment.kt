ff8f4a16864853027fb40f05bb435dc03b5a3094
Sanchith Hegde <sanchith.hegde01@gmail.com>
2022-02-02 23:28:34 +0530

    build(deps): Update deps
    Signed-off-by: Sanchith Hegde <sanchith.hegde01@gmail.com>

--- android/app/src/main/kotlin/ui/MessagesFragment.kt
+++ android/app/src/main/kotlin/ui/MessagesFragment.kt
@@ -8,7 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
 import androidx.navigation.ui.onNavDestinationSelected
 import dagger.hilt.android.AndroidEntryPoint
 import me.sanchithhegde.wastecollection.R
